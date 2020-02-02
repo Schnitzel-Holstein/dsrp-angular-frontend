d67833a812bb33d0ce2a906570bab733bbf78401
Diego Aquino <diego.aquinox@gmail.com>
2020-02-02 03:56:40 -0500

    base of admin panel. Add Bootstrap and jQuery

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    (function($) {
+
+      "use strict";
+    
+      var fullHeight = function() {
+    
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function(){
+          $('.js-fullheight').css('height', $(window).height());
+        });
+    
+      };
+      fullHeight();
+    
+      $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+      });
+    
+    })(jQuery);
+  }
+}
